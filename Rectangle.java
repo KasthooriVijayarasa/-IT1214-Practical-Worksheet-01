2108b29b288a00497c7cae355ce95f4a8b9487bd
KasthooriVijayarasa <selvivijay0904@gmail.com>
2025-05-14 20:04:27 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+class Rectangle{
+	private int length;
+	private int breadth;
+	Rectangle(){
+		this.length=length;
+		this.breadth=breadth;
+	}
+	Rectangle(int length,int breadth){
+		this.length= length;
+		this.breadth=breadth;
+	}
+	public int area(){
+		return length*breadth;
+	}
+}
